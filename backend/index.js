096a71eb83cf7a8956d71e7a007db5d7b13df0b2
sebaborjas <108552013+sebaborjas@users.noreply.github.com>
2022-08-01 19:00:12 -0300

    Connecting DB with express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import conectarDB from "./config/db.js";
+import dotenv from "dotenv/config";
 
 const app = express();
+
+conectarDB();
 
 app.use("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-app.listen(4000, () => {
-  console.log("Servidor funcionando en el puerto 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
