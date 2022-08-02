b44d0c94bb9a855faa28f3dc2676f72ce25f85da
sebaborjas <108552013+sebaborjas@users.noreply.github.com>
2022-08-02 14:39:13 -0300

    Working with register request

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,10 +1,23 @@
-const registrar = (req, res) => {
-  const { nombre, email, password } = req.body;
-  console.log(nombre);
-  console.log(email);
-  console.log(password);
+import Veterinario from "../models/Veterinario.js";
 
-  res.json({ msg: "Registrando usuario" });
+const registrar = async (req, res) => {
+  const { email } = req.body;
+
+  //Prevenir usuarios duplicados
+  const existeUsuario = await Veterinario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    //Guardar un nuevo Veterinario
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const perfil = (req, res) => {
