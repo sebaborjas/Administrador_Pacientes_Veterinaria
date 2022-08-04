9a798acd72327a14c4f81ed0b5c62b247f3b6a2c
sebaborjas <108552013+sebaborjas@users.noreply.github.com>
2022-08-04 15:20:57 -0300

    Conecting frontend with backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from "dotenv/config";
+import cors from "cors"
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
 
@@ -9,6 +10,21 @@ const app = express();
 app.use(express.json()); //Como le envio los datos al server
 
 conectarDB();
+
+const dominiosPermitidos = ["http://127.0.0.1:5173"];
+
+const corsOptions = {
+  origin: function(origin,callback){
+    if(dominiosPermitidos.indexOf(origin) !== -1){
+      //El origen del Request esta permitido
+      callback(null, true);
+    } else {
+      callback( new Error("No permitido por CORS"))
+    }
+  }
+}
+
+app.use(cors( corsOptions ));
 
 app.use("/api/veterinarios", veterinarioRoutes);
 
